a6a693594bc6f5338723ed9d8d990175ae307386
Ace Ory <aceoryx1@gmail.com>
2026-05-20 01:23:25 +0100

    Move DB credentials to environment variables

--- RestAPI/db.js
+++ RestAPI/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 
 // Create MySQL database connection using local database credentials
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Root1532!",
-  database: "FootballDB"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 // Attempt database connection and display connection status
@@ -14,6 +14,7 @@ db.connect((err) => {
     console.error("Database connection failed:", err);
     return;
   }
+
   console.log("Connected to MySQL database");
 });
 
