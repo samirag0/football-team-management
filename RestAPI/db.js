9751d25182863892d7cc1f10a311f99b41d97be9
Ace Ory <aceoryx1@gmail.com>
2026-05-20 01:56:38 +0100

    Configure cloud database connection

--- RestAPI/db.js
+++ RestAPI/db.js
@@ -4,9 +4,13 @@ require("dotenv").config();
 // Create MySQL database connection using local database credentials
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Attempt database connection and display connection status
